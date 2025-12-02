f0fe9f711ff88f6c2f28d314e918b1c130c9b598
asolima1 <asolima1@ualberta.ca>
2025-12-01 18:08:38 -0700

    smaller font

--- src/components/NextPrayerPanel.tsx
+++ src/components/NextPrayerPanel.tsx
@@ -133,10 +133,10 @@ export default function NextPrayerPanel({ prayerTimes }: Props) {
     </h3>
 
         {/* COUNTDOWN: HH : MM : SS with labels underneath */}
-        <div className="-translate-x-13 flex justify-center items-end gap-4 md:gap-6 font-mono mb-10">
+        <div className=" -translate-x-13 flex justify-center items-end gap-4 md:gap-6 font-mono mb-10">
           {/* HOURS */}
           <div className="min-w-[110px]">
-            <div className="-translate-x-11 text-6xl md:text-[270px] font-extrabold">{hh}</div>
+            <div className="-translate-x-11 text-6xl md:text-[230px] font-extrabold">{hh}</div>
             <div className="text-sm md:text-[30px] ml-8 tracking-[0.4em] mt-3 bg-white/10 rounded-xl px-4 py-2 inline-block">
               HOURS
             </div>
@@ -149,7 +149,7 @@ export default function NextPrayerPanel({ prayerTimes }: Props) {
 
           {/* MINUTES */}
           <div className="min-w-[85px]">
-            <div className="-translate-x-8 text-6xl md:text-[270px] font-extrabold">{mm}</div>
+            <div className="-translate-x-8 text-6xl md:text-[230px] font-extrabold">{mm}</div>
             <div className="text-sm md:text-[30px] tracking-[0.4em] mt-3 bg-white/10 rounded-xl px-4 py-2 inline-block">
               MINUTES
             </div>
@@ -162,7 +162,7 @@ export default function NextPrayerPanel({ prayerTimes }: Props) {
 
           {/* SECONDS */}
           <div className="min-w-[110px]">
-            <div className="-translate-x-3 text-6xl md:text-[270px] mr-8 font-extrabold">{ss}</div>
+            <div className="-translate-x-3 text-6xl md:text-[230px] mr-8 font-extrabold">{ss}</div>
             <div className="text-sm md:text-[30px] tracking-[0.4em] mt-3 bg-white/10 rounded-xl px-4 py-2 inline-block">
               SECONDS
             </div>
